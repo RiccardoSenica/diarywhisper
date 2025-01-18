410234d2cbd48ef841b915873b62b62fc17e5d10
Riccardo Senica <riccardo@frompixels.com>
2025-01-18 18:15:00 +0100

    fix: correct env variable

--- utils/handler.ts
+++ utils/handler.ts
@@ -10,7 +10,7 @@ export class ShortcutsHandler {
 
   validateRequest(req: ShortcutsRequest): boolean {
     try {
-      const isValidUser = req.apiKey === process.env.USER_KEY;
+      const isValidUser = req.apiKey === process.env.API_KEY;
       return !!isValidUser;
     } catch (error) {
       console.error('Error validating request:', error);
